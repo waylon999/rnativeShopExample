cb5be10a447954e12e566e31915b5f5764715902
Bryan Wyman <bryan.wyman@gmail.com>
2023-11-01 13:55:08 -0700

    Added product details page
    Allows users to click on a product from the products page and
    be taken to the details for that product.

--- myShop/app/api/api.ts
+++ myShop/app/api/api.ts
@@ -38,3 +38,19 @@ export async function fetchProducts(): Promise<Product[]> {
         throw error;
     }
 }
+
+
+export async function fetchProduct(id: number): Promise<Product> {
+    try {
+        console.log(`${API_URL}/products`);
+        const response = await fetch(`${API_URL}/products/${id}`);
+        if (!response.ok) {
+            throw new Error('Failed to fetch products');
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error fetching products:', error);
+        throw error;
+    }
+}
